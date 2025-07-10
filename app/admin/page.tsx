42f176dd0b676b13c672d7e9eba31bac88cb2bbb
Abidh Muhammed <abidh.kuzhumbil-muhammed@flaconi.de>
2025-07-10 14:12:03 +0200

    fix protocol

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,10 +11,14 @@ import { Badge } from "lucide-react";
 
 export const revalidate = 100;
 
+const protocol = process.env.VERCEL_ENV === "development" ? "http" : "https";
 export default async function AdminPage() {
-  const response = await fetch(`${process.env.VERCEL_URL}/api/lotteries`, {
-    next: { tags: ["lotteries"] },
-  });
+  const response = await fetch(
+    `${protocol}://${process.env.VERCEL_URL}/api/lotteries`,
+    {
+      next: { tags: ["lotteries"] },
+    }
+  );
 
   const lotteries: Lottery[] = await response.json();
 
